f21c6c4a9d157972792cb2a2ddda23bf2d937896
renehu <rene.hu@outlook.com>
2025-08-14 19:57:57 +0800

    flex layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import ProgressBar from "./components/ProgressBar";
-import ProgressBarFunction from "./components/ProgressBarFunction";
-import ProgressBar2Step from "./components/ProgressBar2Step";
+import ProgressBar3 from "./components/ProgressBar3";
 import "./App.css";
 
 class App extends React.Component {
@@ -23,9 +21,15 @@ class App extends React.Component {
     const labels = ["Step1", "Step2", "Step3", "Step4", "Step5", "Step6"];
     return (
       <>
-        <div style={{ width: 600, margin: "40px auto" }}>
-          React 2 step
-          <ProgressBar2Step
+        <div
+          style={{
+            width: "100%",
+            maxWidth: "500px",
+            margin: "40px auto",
+          }}
+        >
+          React ProgressBar3
+          <ProgressBar3
             currentStep={this.state.step}
             totalSteps={6}
             labels={labels}
@@ -43,46 +47,6 @@ class App extends React.Component {
             </button>
           </div>
         </div>
-        {/* <div style={{ width: 600, margin: "40px auto" }}>
-          React Class
-          <ProgressBar
-            currentStep={this.state.step}
-            totalSteps={6}
-            labels={labels}
-          />
-          <div style={{ marginTop: 20, textAlign: "center" }}>
-            <button onClick={this.prev} disabled={this.state.step === 1}>
-              Back
-            </button>
-            <button
-              onClick={this.next}
-              disabled={this.state.step === 6}
-              style={{ marginLeft: 12 }}
-            >
-              Next
-            </button>
-          </div>
-        </div> */}
-        {/* <div style={{ width: 600, margin: "40px auto" }}>
-          React Function
-          <ProgressBarFunction
-            currentStep={this.state.step}
-            totalSteps={6}
-            labels={labels}
-          />
-          <div style={{ marginTop: 20, textAlign: "center" }}>
-            <button onClick={this.prev} disabled={this.state.step === 1}>
-              Back
-            </button>
-            <button
-              onClick={this.next}
-              disabled={this.state.step === 6}
-              style={{ marginLeft: 12 }}
-            >
-              Next
-            </button>
-          </div>
-        </div> */}
       </>
     );
   }
